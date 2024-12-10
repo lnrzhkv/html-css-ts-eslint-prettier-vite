54a1625a71ffdf8339ec2b629bf9aa791a954e40
Pavel <p.melnik@intelico.su>
2024-12-10 13:13:05 +0300

    add progress step

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,41 @@
 import { $ } from './_lib';
-import { initTheme, toggleTheme } from './theme';
 
 document.addEventListener('DOMContentLoaded', () => {
-	initTheme();
-	$('[data-button="toggle-theme"]').on('click', toggleTheme);
+	const circles = $('[data-progress="circle"]').get();
+
+	const line = $('[data-progress="line"]').get();
+
+	let currentActive = 0;
+
+	$('[data-button="prev"]').on('click', () => {
+		currentActive--;
+		hasControls();
+
+		update();
+	});
+
+	$('[data-button="next"]').on('click', () => {
+		currentActive++;
+		hasControls();
+
+		update();
+	});
+
+	function update() {
+		circles.forEach((c, index) => {
+			if (index < currentActive) {
+				c.classList.add('active');
+			} else {
+				c.classList.remove('active');
+			}
+		});
+
+		line[0].style.width = `${(currentActive / circles.length) * 100}%`;
+	}
+
+	function hasControls() {
+		$('[data-button]').removeClass('disabled');
+		if (currentActive <= 0) $('[data-button="prev"]').addClass('disabled');
+		if (circles.length - 1 < currentActive) $('[data-button="next"]').addClass('disabled');
+	}
 });
