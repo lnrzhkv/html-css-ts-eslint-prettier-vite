244b9a80e7608f8ee51a190aa00ae8f94575500a
Alina <lnrzhkv3367337@gmail.com>
2024-12-11 15:56:05 +0300

    feat: add dynamic functional

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,4 +2,11 @@ import { $ } from './_lib';
 
 document.addEventListener('DOMContentLoaded', () => {
 	// code there
+	$('.panel').on('click', (e) => {
+		$('.panel').removeClass('active');
+
+		if (e.currentTarget) {
+			e.currentTarget.classList.add('active');
+		}
+	});
 });
